8d0f386e8df3975602e2234eae1a80231fd035ca
Michael Zhou <yizhi@taobao.com>
2011-10-25 19:13:59 -0600

    修正版权注解

--- antx/autoconfig/src/main/java/org/w3c/tidy/Lexer.java
+++ antx/autoconfig/src/main/java/org/w3c/tidy/Lexer.java
@@ -13,8 +13,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
+
 /*
  * @(#)Lexer.java   1.11 2000/08/16
  *
