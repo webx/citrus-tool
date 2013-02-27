f67995d877ed8bb3d0c90b6acb36a4f4018f48db
Michael Zhou <yizhi@taobao.com>
2013-02-27 12:28:24 -0600

    删除无用的方法

--- eclipse/springext-eclipse-plugin/src/main/java/com/alibaba/ide/plugin/eclipse/springext/util/SpringExtPluginUtil.java
+++ eclipse/springext-eclipse-plugin/src/main/java/com/alibaba/ide/plugin/eclipse/springext/util/SpringExtPluginUtil.java
@@ -7,129 +7,27 @@ import static com.alibaba.citrus.util.StringUtil.*;
 import java.io.IOException;
 import java.net.URL;
 
-import org.eclipse.core.filebuffers.FileBuffers;
-import org.eclipse.core.filebuffers.ITextFileBuffer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.IRegion;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IFileEditorInput;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.wst.sse.core.StructuredModelManager;
-import org.eclipse.wst.sse.core.internal.provisional.IStructuredModel;
-import org.eclipse.wst.sse.core.utils.StringUtils;
 import org.springframework.core.io.Resource;
 
 import com.alibaba.citrus.springext.SourceInfo;
 import com.alibaba.ide.plugin.eclipse.springext.SpringExtPlugin;
 
-@SuppressWarnings("restriction")
 public class SpringExtPluginUtil {
-    private static final String JAR_FILE_PROTOCOL = "jar:file:"; //$NON-NLS-1$
-
-    /**
-     * 取得指定document所在的project。
-     * <p/>
-     * 参考实现：
-     * {@link org.eclipse.jst.jsp.ui.internal.hyperlink.XMLJavaHyperlinkDetector#createHyperlink(String, IRegion, IDocument)}
-     */
-    public static IProject getProjectFromDocument(IDocument document) {
-        // try file buffers
-        ITextFileBuffer textFileBuffer = FileBuffers.getTextFileBufferManager().getTextFileBuffer(document);
-
-        if (textFileBuffer != null) {
-            IPath basePath = textFileBuffer.getLocation();
-
-            if (basePath != null && !basePath.isEmpty()) {
-                IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(basePath.segment(0));
-
-                if (basePath.segmentCount() > 1 && project.isAccessible()) {
-                    return project;
-                }
-            }
-        }
-
-        // fallback to SSE-specific knowledge
-        IStructuredModel model = null;
-
-        try {
-            model = StructuredModelManager.getModelManager().getExistingModelForRead(document);
-
-            if (model != null) {
-                String baseLocation = model.getBaseLocation();
-
-                // URL fixup from the taglib index record
-                if (baseLocation.startsWith("jar:/file:")) { //$NON-NLS-1$
-                    baseLocation = StringUtils.replace(baseLocation, "jar:/", "jar:"); //$NON-NLS-1$ //$NON-NLS-2$
-                }
-
-                /*
-                 * Handle opened TLD files from JARs on the Java Build Path by
-                 * finding a package fragment root for the same .jar file and
-                 * opening the class from there. Note that this might be from a
-                 * different Java project's build path than the TLD.
-                 */
-                if (baseLocation.startsWith(JAR_FILE_PROTOCOL)
-                        && baseLocation.indexOf('!') > JAR_FILE_PROTOCOL.length()) {
-                    String baseFile = baseLocation.substring(JAR_FILE_PROTOCOL.length(), baseLocation.indexOf('!'));
-                    IPath basePath = new Path(baseFile);
-                    IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
-
-                    for (IProject project : projects) {
-                        try {
-                            if (project.isAccessible() && project.hasNature(JavaCore.NATURE_ID)) {
-                                IJavaProject javaProject = JavaCore.create(project);
-
-                                if (javaProject.exists()) {
-                                    IPackageFragmentRoot root = javaProject.findPackageFragmentRoot(basePath);
-
-                                    if (root != null) {
-                                        return javaProject.getProject();
-                                    }
-                                }
-                            }
-                        } catch (CoreException ignored) {
-                        }
-                    }
-                } else {
-                    IPath basePath = new Path(baseLocation);
-
-                    if (basePath.segmentCount() > 1) {
-                        IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(basePath.segment(0));
-
-                        if (project != null && project.isAccessible()) {
-                            return project;
-                        }
-                    }
-                }
-            }
-        } finally {
-            if (model != null) {
-                model.releaseFromRead();
-            }
-        }
-
-        // Try get project from editor input
-        IEditorInput input = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor()
-                .getEditorInput();
-
-        return getProjectFromInput(input);
-    }
-
     public static IProject getProjectFromInput(IEditorInput input) {
         return getProjectFromInput(input, true);
     }
