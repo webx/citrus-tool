5dff3973a1b7a936f11bb13e75c9d992261ddb3b
Michael Zhou <yizhi@taobao.com>
2014-04-03 23:59:36 -0600

    Bugfix: 最新版的eclipse中，有时不能compute schema，因为查找到了pom project中。

--- eclipse/springext-eclipse-plugin/src/main/java/com/alibaba/ide/plugin/eclipse/springext/resolver/SpringExtSchemaResolver.java
+++ eclipse/springext-eclipse-plugin/src/main/java/com/alibaba/ide/plugin/eclipse/springext/resolver/SpringExtSchemaResolver.java
@@ -1,16 +1,20 @@
 package com.alibaba.ide.plugin.eclipse.springext.resolver;
 
-import static com.alibaba.citrus.util.StringUtil.*;
-import static com.alibaba.ide.plugin.eclipse.springext.resolver.SpringExtURLUtil.*;
+import static com.alibaba.citrus.util.StringUtil.isBlank;
+import static com.alibaba.citrus.util.StringUtil.trimToNull;
+import static com.alibaba.ide.plugin.eclipse.springext.resolver.SpringExtURLUtil.getProjectFromURL;
+import static com.alibaba.ide.plugin.eclipse.springext.resolver.SpringExtURLUtil.toSpringextURL;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.wst.common.uriresolver.internal.provisional.URIResolverExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.alibaba.citrus.springext.Schema;
 import com.alibaba.ide.plugin.eclipse.springext.schema.SchemaResourceSet;
+import com.alibaba.ide.plugin.eclipse.springext.util.SpringExtPluginUtil;
 
 @SuppressWarnings("restriction")
 public class SpringExtSchemaResolver implements URIResolverExtension {
@@ -46,8 +50,23 @@ public class SpringExtSchemaResolver implements URIResolverExtension {
     private IProject getProject(IFile file, String baseLocation) {
         IProject project = null;
 
+        // 优先查找Java Project
+        // 情况解释：Eclipse最近的更新导致，一个file可能会找到parent pom project，这时就无法生成classpath。
+        // 这里优先找出file对应的java project，以确保schema set可读出。
+        IFile[] files = ResourcesPlugin.getWorkspace().getRoot().findFilesForLocationURI(file.getLocationURI());
+
+        if (files != null) {
+            for (IFile f : files) {
+                if (SpringExtPluginUtil.getJavaProject(f.getProject(), false) != null) {
+                    file = f;
+                    break;
+                }
+            }
+        }
+
         if (file != null) {
             project = file.getProject();
+
         }
 
         if (project == null) {
